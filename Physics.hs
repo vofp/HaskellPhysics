91a5293dade5cd6cb50f9ffbe533298394bd00ba
vofp <vof@onid.orst.edu>
2014-11-22 10:01:59 -0800

    added settings

--- Physics.hs
+++ Physics.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances, FlexibleInstances #-}
 
 module Physics where
 
 import Data.List
 
 type AccelFunc    = Double -> AccelVector
+type Setting      = Element -> AccelFunc
 type Vector       = (Double,Double,Double)
 type AccelVector  = (Double,Double,Double)
 type LogType      = String
@@ -15,7 +16,7 @@ type Size         = Double
 type Time         = Double
 type Velocity     = (Double,Double,Double)
 
-data Env a  = Env Time [a] 
+data Env a  = Env Time [a] [Setting]
 data Log    = LogColSS Time Sphere Sphere
             | LogColSW Time Sphere Wall
 data Sphere = Sphere Name Size Position Velocity AccelFunc
@@ -34,7 +35,7 @@ class Object a where
     normalize  :: a -> (Name, Size, Position, Velocity)
 
     getObjName :: Name -> Env a -> Maybe a
-    getObjName s (Env t a) = getObjNamelist s a
+    getObjName s (Env t a _) = getObjNamelist s a
 
     getObjNamelist :: Name -> [a] -> Maybe a
     getObjNamelist s []     = Nothing
@@ -56,15 +57,7 @@ class Object a where
     getAccelObj :: Name -> Env a -> Maybe AccelFunc
     getAccelObj = getInfoObj getAccel
 
-    stepTime :: Time -> Time -> a -> a
-    stepTime t1 t2 p = update newPos newVelo p 
-        where f          = getAccel p
-              (ax,ay,az) = avgAccel t1 t2 4 f
-              t          = t2 - t1
-              (px,py,pz) = getPos p
-              (vx,vy,vz) = getVelo p
-              newPos     = (px+vx*t+0.5*ax*t*t,py+vy*t+0.5*ay*t*t,pz+vz*t+0.5*az*t*t)
-              newVelo    = (vx+ax*t,vy+ay*t,vz+az*t)
+    stepTime :: Time -> Time -> [Setting] -> a -> a
 
 instance Object Element where
     getAccel (Sp s)   = getAccel s
@@ -82,6 +75,16 @@ instance Object Element where
     update p v (Sp s) = Sp (update p v s)
     update p v (Wa s) = Wa (update p v s)
 
+    stepTime t1 t2 s p = update newPos newVelo p 
+        where f          = getAccel p
+              t          = t2 - t1
+              a          = avgAccel t1 t2 4 f
+              (ax,ay,az) = simpleCombine (foldr simpleCombine (0,0,0) [g p t | g <- s]) a
+              (px,py,pz) = getPos p
+              (vx,vy,vz) = getVelo p
+              newPos     = stepPos t (px,py,pz) (vx,vy,vz) (ax,ay,az)
+              newVelo    = (vx+ax*t,vy+ay*t,vz+az*t)
+
 instance Object Sphere where
     -- new = Sphere
     update p v (Sphere n s _ _ f) = (Sphere n s p v f)
@@ -91,6 +94,16 @@ instance Object Sphere where
     getVelo (Sphere _ _ _ v _) = v
     getAccel (Sphere _ _ _ _ f) = f
     normalize (Sphere n s p v _) = (n, s, p, v)
+
+    stepTime t1 t2 s p = update newPos newVelo p 
+        where f          = getAccel p
+              t          = t2 - t1
+              a          = avgAccel t1 t2 4 f
+              (ax,ay,az) = simpleCombine (foldr simpleCombine (0,0,0) [g (Sp p) t | g <- s]) a
+              (px,py,pz) = getPos p
+              (vx,vy,vz) = getVelo p
+              newPos     = stepPos t (px,py,pz) (vx,vy,vz) (ax,ay,az)
+              newVelo    = (vx+ax*t,vy+ay*t,vz+az*t)
 
 instance Object Wall where
     -- new  n s p v _ = Wall  n s p v
@@ -102,6 +115,16 @@ instance Object Wall where
     getAccel (Wall _ _ _ _) = \x -> (0.0,0.0,0.0)
     normalize (Wall n s p v) = (n, s, p, v)
 
+    stepTime t1 t2 s p = update newPos newVelo p 
+        where f          = getAccel p
+              t          = t2 - t1
+              a          = avgAccel t1 t2 4 f
+              (ax,ay,az) = simpleCombine (foldr simpleCombine (0,0,0) [g (Wa p) t | g <- s]) a
+              (px,py,pz) = getPos p
+              (vx,vy,vz) = getVelo p
+              newPos     = stepPos t (px,py,pz) (vx,vy,vz) (ax,ay,az)
+              newVelo    = (vx+ax*t,vy+ay*t,vz+az*t)
+
 magnitude :: (Double,Double,Double) -> (Double,Double,Double) -> Double
 magnitude (a,b,c) (x,y,z) = sqrt (i*i + j*j + k*k)
                                 where i = x - a
@@ -112,25 +135,25 @@ class Collision a b where
     collision :: a -> b -> Bool
 
 -- | See if Collision happened between 2 spheres
--- >>> collision (Sphere "Test" 1 (0,0,0) (0,0,0) gravityVelo) (Sphere "Test2" 1 (1,0,0) (0,0,0) gravityVelo)
+-- >>> collision (Sphere "Test" 1 (0,0,0) (0,0,0) nothingVelo) (Sphere "Test2" 1 (1,0,0) (0,0,0) nothingVelo)
 -- True
 -- 
--- >>> collision (Sphere "Test" 1 (0,0,0) (0,0,0) gravityVelo) (Sphere "Test2" 1 (2,0,0) (0,0,0) gravityVelo)
+-- >>> collision (Sphere "Test" 1 (0,0,0) (0,0,0) nothingVelo) (Sphere "Test2" 1 (2,0,0) (0,0,0) nothingVelo)
 -- True
 -- 
--- >>> collision (Sphere "Test" 1 (0,0,0) (0,0,0) gravityVelo) (Sphere "Test2" 1 (3,0,0) (0,0,0) gravityVelo)
+-- >>> collision (Sphere "Test" 1 (0,0,0) (0,0,0) nothingVelo) (Sphere "Test2" 1 (3,0,0) (0,0,0) nothingVelo)
 -- False
 instance Collision Sphere Sphere where
     collision (Sphere _ s1 p1 _ _) (Sphere _ s2 p2 _ _) = magnitude p1 p2 <= s1 + s2
 
 -- | See if Collision happened between a sphere and wall
--- >>> collision (Sphere "Test" 1 (2,0,0) (0,0,0) gravityVelo) (Wall "Test2" 1 (0,0,0) (1,0,0))
+-- >>> collision (Sphere "Test" 1 (2,0,0) (0,0,0) nothingVelo) (Wall "Test2" 1 (0,0,0) (1,0,0))
 -- False
 -- 
--- >>> collision (Sphere "Test" 1 (2,0,0) (0,0,0) gravityVelo) (Wall "Test2" 1 (0,0,0) (0,1,0))
+-- >>> collision (Sphere "Test" 1 (2,0,0) (0,0,0) nothingVelo) (Wall "Test2" 1 (0,0,0) (0,1,0))
 -- True
 -- 
--- >>> collision (Sphere "Test" 1 (2,0,0) (0,0,0) gravityVelo) (Wall "Test2" 1 (0,0,0) (0,0,1))
+-- >>> collision (Sphere "Test" 1 (2,0,0) (0,0,0) nothingVelo) (Wall "Test2" 1 (0,0,0) (0,0,1))
 -- True
 instance Collision Sphere Wall where
     collision (Sphere _ s1 (x,y,z) _ _) (Wall _ _ (x0,y0,z0) (a,b,c)) =  (a*x+b*y+c*z+d)/sqrt(a*a+b*b+c*c) <= s1
@@ -142,8 +165,8 @@ instance Collision Wall Sphere where
 class Combine a where
     simpleCombine :: a -> a -> a
 
-instance Combine Env where
-    simpleCombine (Env _ a1) (Env _ a2) = Env 0 (a1++a2)
+instance Combine (Env a) where
+    simpleCombine (Env _ a1 f1) (Env _ a2 f2) = Env 0 (a1++a2) (f1++f2)
 
 instance Combine Sphere where
     simpleCombine (Sphere n1 s1 p1 v1 f1) (Sphere n2 s2 p2 v2 f2) = Sphere n s p v f
@@ -153,12 +176,18 @@ instance Combine Sphere where
               v = simpleCombine v1 v2
               f = (\x -> simpleCombine (f1 x) (f2 x))
 
-
+-- | Combine vectors
+-- >>> let s = [gravityVelo]
+-- >>> let p = Sp (Sphere "Test" 1 (0,0,0) (0,0,0) nothingVelo)
+-- >>> let a = (0.0,0.0,0.0)
+-- >>> let t = 1.0
+-- >>> simpleCombine (foldr simpleCombine (0,0,0) [g p t | g <- s]) a
+-- (0.0,0.0,9.8)
 instance Combine Vector where
-    simpleCombine (a,b,c) (x,y,z) = ((a+x)/2,(b+y)/2,(c+z)/2)
+    simpleCombine (a,b,c) (x,y,z) = (a+x,b+y,c+z)
 
 -- | Showing the Sphere
--- >>> Sphere "Test" 1 (0,0,0) (0,0,0) gravityVelo
+-- >>> Sphere "Test" 1 (0,0,0) (0,0,0) nothingVelo
 -- Sphere Test 1.0 (0.0,0.0,0.0) (0.0,0.0,0.0)
 
 instance Show Sphere where
@@ -175,7 +204,7 @@ instance Show Element where
     show (Wa w) = show w 
 
 instance Show a => Show (Env a) where
-    show (Env t a) = "Env " ++ show t ++ " " ++ show a
+    show (Env t a _) = "Env " ++ show t ++ " " ++ show a
 
 instance Eq Sphere where
     (Sphere n1 s1 p1 v1 _) == (Sphere n2 s2 p2 v2 _) = n1 == n2 
@@ -196,7 +225,7 @@ instance Ord Sphere where
 
 
 instance Ord a => Eq (Env a) where
-    (Env t a) == (Env t2 a2) = t == t2 && sort a == sort a2
+    (Env t a _) == (Env t2 a2 _) = t == t2 && sort a == sort a2
 
 -- | Create a basic acceleration function with a vector
 -- >>> (accelFunc (1,1,1)) 2
@@ -205,13 +234,20 @@ accelFunc :: AccelVector -> AccelFunc
 accelFunc (a,b,c) = \t -> (t*a,t*b,t*c)
 
 -- | Basic gravity function
--- >>> gravityVelo 100
+-- >>> gravityVelo (Sp (Sphere "Test" 1 (2,0,0) (0,0,0) nothingVelo)) 100
 -- (0.0,0.0,9.8)
-gravityVelo :: AccelFunc
-gravityVelo = \t -> (0,0,9.8)
+gravityVelo :: Setting
+gravityVelo (Sp _) = \t -> (0,0,9.8)
+gravityVelo (Wa _) = \t -> (0,0,0.0)
+
+-- | Basic not accelerating function
+-- >>> nothingVelo 100
+-- (0.0,0.0,0.0)
+nothingVelo :: AccelFunc
+nothingVelo = \t -> (0,0,0)
 
 -- | Find the avg Acceleration vector over a period of time 
--- >>> avgAccel 0 2 2 gravityVelo
+-- >>> avgAccel 0 2 2 (accelFunc (0,0,9.8))
 -- (0.0,0.0,9.8)
 avgAccel :: Time -> Time -> Int -> AccelFunc -> AccelVector
 avgAccel t1 t2 i f = avg [f t | let rt = (t2-t1)/(fromIntegral i), j <- [0..i], let t = t1 + rt * (fromIntegral j)]
@@ -224,8 +260,8 @@ avg v = foldr (\(a,b,c) (x,y,z) -> (a/l+x,b/l+y,c/l+z)) (0,0,0) v
       where l = fromIntegral (length v)
 
 -- | Step the time of the Env
--- >>> let s = Sphere "Test" 1 (0,0,0) (0,0,0) gravityVelo
--- >>> let e = Env 0 [s]
+-- >>> let s = Sphere "Test" 1 (0,0,0) (0,0,0) nothingVelo
+-- >>> let e = Env 0 [s] [gravityVelo]
 -- >>> let e1 = stepTimeEnv 1 e
 -- >>> e1
 -- Env 1.0 [Sphere Test 1.0 (0.0,0.0,4.9) (0.0,0.0,9.8)]
@@ -237,22 +273,22 @@ avg v = foldr (\(a,b,c) (x,y,z) -> (a/l+x,b/l+y,c/l+z)) (0,0,0) v
 -- >>> stepTimeEnv 1 e1 == e2
 -- True
 -- 
--- >>> let s2 = Sphere "Test2" 1 (2,0,0) (0,0,0) gravityVelo
+-- >>> let s2 = Sphere "Test2" 1 (2,0,0) (0,0,0) nothingVelo
 -- >>> let w = Wall "Test3" 1 (0,0,0) (1,0,0)
--- >>> let e3 = Env 0 [(Sp s2),(Wa w)]
+-- >>> let e3 = Env 0 [(Sp s2),(Wa w)] [gravityVelo]
 -- >>> let e4 = stepTimeEnv 1 e3
 -- >>> e4
 -- Env 1.0 [Sphere Test2 1.0 (2.0,0.0,4.9) (0.0,0.0,9.8),Wall Test3 1.0 (0.0,0.0,0.0) (1.0,0.0,0.0)]
 stepTimeEnv :: Object a => Time -> Env a -> Env a
-stepTimeEnv st (Env t1 a) = Env t2 (map (stepTime t1 t2) a)
+stepTimeEnv st (Env t1 a f) = Env t2 (map (stepTime t1 t2 f) a) f
                           where t2 = t1 + st
 
 stepPos :: Time -> Position -> Velocity -> AccelVector -> Position
-stepPos t (px,py,pz) (vx,vy,vz) (ax,ay,az) = (px+vx*t+ax*t*t,py+vy*t+ay*t*t,pz+vz*t+az*t*t)
+stepPos t (px,py,pz) (vx,vy,vz) (ax,ay,az) = (px+vx*t+0.5*ax*t*t,py+vy*t+0.5*ay*t*t,pz+vz*t+0.5*az*t*t)
 
 -- | Get an Sphere from an Env
--- >>> let s = Sphere "Test" 1 (0,0,0) (0,0,0) gravityVelo
--- >>> let e = Env 0 [s]
+-- >>> let s = Sphere "Test" 1 (0,0,0) (0,0,0) nothingVelo
+-- >>> let e = Env 0 [s] [gravityVelo]
 -- >>> getObjName "Test" e
 -- Just Sphere Test 1.0 (0.0,0.0,0.0) (0.0,0.0,0.0)
 -- 
